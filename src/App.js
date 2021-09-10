c3d713e2f8e4cc957b0da27169c1718d8616c504
Nick Loucks <nick.d.loucks@gmail.com>
2021-09-10 13:48:54 -0700

    convert to class-based component, +React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-         Stopwatch *header*
-      </header>
-      <button>Start</button>
-      <button>Stop</button>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+
+    return (
+     <React.Fragment className="App">
+        {/*<header className="App-header"> {/*re-work header and styling*/}
+           <h1>Stopwatch *header*</h1>
+           <span>text currently only shows up in header element in App.js...</span>
+           <button>Start</button>
+        {/*</header>*/}
+        <p>why does this not display? the rest of content must have been getting covered/ignored ny app.css app-header class</p>
+        <button>Stop</button>
+        <button>Reset</button>
+        <button>Save</button>  
+     </React.Fragment>
+    );
+  }
 }
 
 export default App;
