ed0bb971d961559a22435535ce4a198a0c10d2e8
Nick Loucks <nick.d.loucks@gmail.com>
2021-09-13 20:32:38 -0700

    outlining delete function for List component

--- src/list.js
+++ src/list.js
@@ -4,10 +4,20 @@ import {default as Timer} from './timer.js';
 class List extends React.Component {
     constructor(props){
         super(props);
+        this.deleteTime = this.deleteTime.bind(this);
+    }
+
+    deleteTime(){
+        // delete the record from the list
     }
 
     render(){
-        const times = this.props.times.map(time => <li>{<Timer timeElapsed={time}/>}</li>);
+        const times = this.props.times.map(time => 
+            <li>{<Timer timeElapsed={time} />}
+                <button onClick = {this.deleteTime}>&nbsp;X&nbsp;</button>
+            </li>);
+            // make sure the list can pull both the time and it's stamp identifier: 
+                        // add stamp parameter to .map() callback somehow
             // handle edge cases where list is empty
             // sort times by stortest to longest
             // add "personal record" indicator to best time
