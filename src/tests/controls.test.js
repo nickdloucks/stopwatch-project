ba73275e170b886631fc06bf7b0587f7a0346ee9
Nick <nickdloucks@outlook.com>
2021-09-22 20:57:29 -0700

    refactor controls tests (not testing App)

--- src/tests/controls.test.js
+++ src/tests/controls.test.js
@@ -1,26 +1,26 @@
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import Controls from '../constrols.js';
 
 test('renders stopwatch start button', () => {
-  render(<App />);
+  render(<Controls />);
   const startButton = screen.getByText(/start/i);
   expect(startButton).toBeInTheDocument();
 });
 
 test('renders stopwatch stop button', () => {
-  render(<App />);
+  render(<Controls />);
   const stopButton = screen.getByText(/end/i);
   expect(stopButton).toBeInTheDocument();
 });
 
 test('renders stopwatch reset button', () => {
-  render(<App />);
+  render(<Controls />);
   const resetButton = screen.getByText(/reset/i);
   expect(resetButton).toBeInTheDocument();
 });
 
 test('renders stopwatch save button', () => {
-  render(<App />);
+  render(<Controls />);
   const saveButton = screen.getByText(/save/i);
   expect(saveButton).toBeInTheDocument();
 });
