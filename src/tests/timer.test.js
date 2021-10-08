9b9d644cb0bc0e8928e0fd5a6ac42bb8a1320248
Nick <nickdloucks@outlook.com>
2021-10-08 12:03:30 -0700

    working thru tests

--- src/tests/timer.test.js
+++ src/tests/timer.test.js
@@ -1,6 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Timer from '../timer.js';
-import App from '../App'
 
 test('renders timer', ()=>{
     render(<Timer />);
@@ -9,8 +8,17 @@ test('renders timer', ()=>{
     expect(timerElement).toBeInTheDocument();
 });
 
-    test('displays input properly', ()=>{
-    render(<Timer timeElapsed = {3661} />);
+test('displays time input properly', ()=>{
+    render(<Timer timeElapsed = {3661} />); // total seconds equal to: one hour, one minute, one second
     const timerString = screen.getByText('01:01:01');
     expect(timerString).toBeInTheDocument();
+});
+
+test('displays timeStamp properly', ()=>{
+    let now = new Date();
+    let timeStamp = ` Time on ${now.getMonth()+1}-${now.getDate()}-${now.getFullYear()}\
+       at ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    render(<Timer dateStamp= {timeStamp} timeElapsed= {1} />);
+    
+    // finish writing test
 });
