b5d492e470b73f9dd4fc0e4ad6d161e0adaeb16a
Nick <nickdloucks@outlook.com>
2021-10-12 08:32:44 -0700

    experimenting w/ async/await for clocktick

--- src/staging.js
+++ src/staging.js
@@ -1,10 +1,31 @@
 import App from "./App"
 
 function* newSecond(){
-    while (App.state.running){
+    if ((App.state.running) && (this.state.time <= this.props.maxTime)){
         yield App.state.time;
         App.setState({
             time: this.state.time + 1
         });
     }
 }
+
+//////
+
+function waitASec() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(`current time = ${App.state.time}`);
+      }, 990);
+    });
+}
+  
+async function addNewSec() {
+    console.log('calling');
+    const result = await waitASec();
+    console.log(result);
+    result.then(setState({
+        time: App.state.time + 1
+    }))
+}  
+
+asyncCall();
