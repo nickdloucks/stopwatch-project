36788b22c7b60a9e2405ab146c74947f1c113f95
Nick <nickdloucks@outlook.com>
2021-10-12 21:08:48 -0700

    traced bug, move draft code to staging file

--- src/App.js
+++ src/App.js
@@ -91,29 +91,15 @@ class App extends React.Component {
 
       this.waitASec().then(this.render());
       console.log("increment second function should have run");
-
+      // BUG #1 PROBABLY IN WHILE LOOP, EVERYTHING SEEMS TO WORK UNTIL HERE:
     }while ((this.state.running) && (this.state.time <= this.props.maxTime));
       // prevent infinite loop by using the maxTime prop
       // wait 990 milliseconds before incrementing the seconds count
       // 990 is subject to change based on the time complexity/ performance of the app:
       // need to account for the milliseconds it takes to run the program
-    
   }
 
-  componentWillReceiveProps(){
-    //
-  }
-  /*
-  componentDidMount(){ // 1st attempt at kicking off the timer loop,
-      // intending to re-render timer each time a second passes
-      // need to optimize so other components don't re-render unnecessarily
-    if (this.state.running && (this.state.time <= this.props.maxTime)){ // maximum time limit to prevent infinite loop
-      // maxTime is passed as a prop in index.js
-      setInterval(this.setState({   // re-render Timer every time another second has elapsed, until stop button is pressed.
-        time: this.state.time + 1
-      }), 1000)
-    }
-  }*/
+
 
   render(){
 
--- src/staging.js
+++ src/staging.js
@@ -29,3 +29,21 @@ async function addNewSec() {
 }  
 
 asyncCall();
+
+
+
+componentWillReceiveProps(){
+    //
+  }
+
+  /*
+  componentDidMount(){ // 1st attempt at kicking off the timer loop,
+      // intending to re-render timer each time a second passes
+      // need to optimize so other components don't re-render unnecessarily
+    if (this.state.running && (this.state.time <= this.props.maxTime)){ // maximum time limit to prevent infinite loop
+      // maxTime is passed as a prop in index.js
+      setInterval(this.setState({   // re-render Timer every time another second has elapsed, until stop button is pressed.
+        time: this.state.time + 1
+      }), 1000)
+    }
+  }*/
