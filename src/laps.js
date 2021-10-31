d70193b55059ecfd45c448ab6ff78232db39f845
Nick <nickdloucks@outlook.com>
2021-10-31 13:17:24 -0700

    add key to laps list items and export

--- src/laps.js
+++ src/laps.js
@@ -5,9 +5,11 @@ import { render } from '@testing-library/react';
 
 function Laps(lapsArray){
     const LapsList = lapsArray.map(function(time, index){
-        <li>Lap {index + 1}:&nbsp;<Timer timeElapsed = {time}/></li>
-    });
+        <li key={time.date.concat((index + 1).toString())}>Lap {index + 1}:&nbsp;<Timer timeElapsed = {time}/></li>
+    }); // each list item should be a static timer: key is the date plus the lap number
     return(
         <ol>{LapsList}</ol>
     );
 }
+
+export default Laps;
